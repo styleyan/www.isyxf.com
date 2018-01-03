8f5b382f4c38c87a622f6042c463f264eeb8f9e6
yanxiaofei <forget_love2161@126.com>
2018-01-04 01:29:35 +0800

    fix: 归档页面数据嵌套

--- src/views/archive/index.vue
+++ src/views/archive/index.vue
@@ -1,24 +1,16 @@
 <template>
   <article class="post archive">
     <h1 class="title">归档</h1>
-    <div class="entry-content">
-      <h3>2017年09月 (13)</h3>
+    <div class="entry-content" 
+      v-for="(archive, key) in archiveList"
+      :key="key">
+      <h3>{{archive.name}} ({{archive.list.length}})</h3>
       <ul>
-        <li>
-          <a href="/post/something-about-headless-chrome" class="" title="Headless Chrome目前的一些问题">Headless Chrome目前的一些问题</a>&nbsp;
-          <span class="date">2017-09-19</span>
-        </li>
-        <li>
-          <a href="/post/react-native-async" class="" title="React Native的异步">React Native的异步</a>&nbsp;
-          <span class="date">2017-09-12</span>
-        </li>
-        <li>
-          <a href="/post/mac-karabiner-elements-for-keyborad" class="" title="Mac外接键盘利器: karabiner-elements">Mac外接键盘利器: karabiner-elements</a>&nbsp;
-          <span class="date">2017-09-12</span>
-        </li>
-        <li>
-          <a href="/post/android-7.1-nested-scrollview-fling" class="" title="Android N: 修复7.1.1下NestedScrollView滑动失灵的问题 ">Android N: 修复7.1.1下NestedScrollView滑动失灵的问题 </a>&nbsp;
-          <span class="date">2017-09-09</span>
+        <li 
+          v-for="(item, key) in archive.list"
+          :key="key">
+          <a :href="item.url">{{item.title}}</a>&nbsp;
+          <span class="date">{{item.createTime}}</span>
         </li>
       </ul>
     </div>
@@ -26,5 +18,48 @@
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'sssdf',
+  data() {
+    return {
+      archiveList: [
+        {
+          name: '2017年09月',
+          list: [
+            {
+              url: '/post/something-about-headless-chrome',
+              title: 'Headless Chrome目前的一些问题',
+              createTime: '2017-09-12 11:33:21',
+            },
+            {
+              url: '/post/something-about-headless-chrome',
+              title: 'Headless Chrome目前的一些问题',
+              createTime: '2017-09-12 11:33:21',
+            },
+          ],
+        },
+        {
+          name: '2017年09月',
+          list: [
+            {
+              url: '/post/something-about-headless-chrome',
+              title: 'Headless Chrome目前的一些问题',
+              createTime: '2017-09-12 11:33:21',
+            },
+            {
+              url: '/post/something-about-headless-chrome',
+              title: 'Headless Chrome目前的一些问题',
+              createTime: '2017-09-12 11:33:21',
+            },
+            {
+              url: '/post/something-about-headless-chrome',
+              title: 'Headless Chrome目前的一些问题',
+              createTime: '2017-09-12 11:33:21',
+            },
+          ],
+        },
+      ],
+    }
+  },
+}
 </script>
