e0d4f9ce5148fec25f00452b137e9ebfbaf3a6ad
yanxiaofei <forget_love2161@126.com>
2018-01-04 01:07:28 +0800

    feat: 套首页数据

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,15 +1,13 @@
 <template>
   <section id="page-index">
-    <article class="post">
-      <div class="meta">
-        <div class="date">2017-09-12 11:33:21</div>
-      </div>
-      <h1 class="title"><a href="/post/react-native-async" class="">React Native的异步</a></h1>
+    <article class="post" 
+      v-for="(item, key) in aritcleList"
+      :key="key">
+      <div class="meta">{{item.createTime}}</div>
+      <h1 class="title"><a :href="item.url">{{item.title}}</a></h1>
       <div class="entry-content">
-        <div>
-          <p>RN的JSBridge是一个完成度相当高的东西。它带来了开发速度的巨大提升，相应地也导致了运行原生代码时总是异步的这一特征，本文讨论一下RN中异步的一些场景和异步带来的一些问题。</p> 
-        </div>
-        <a href="/post" class="">阅读更多 &raquo;</a>
+        <div v-html="item.html"></div>
+        <a :href="item.url">阅读更多&raquo;</a>
       </div>
     </article>
     <nav class="pagination">
@@ -24,6 +22,26 @@
 <script>
 export default {
   name: 'index-page',
+  data() {
+    return {
+      aritcleList: [{
+        createTime: '2017-09-12 11:33:21',
+        title: 'React Native的异步',
+        html: '<p>RN的JSBridge是一个完成度相当高的东西。它带来了开发速度的巨大提升，相应地也导致了运行原生代码时总是异步的这一特征，本文讨论一下RN中异步的一些场景和异步带来的一些问题。</p>',
+        url: '/post',
+      }, {
+        createTime: '2017-09-12 11:33:21',
+        title: 'React Native的异步',
+        html: '<p>RN的JSBridge是一个完成度相当高的东西。它带来了开发速度的巨大提升，相应地也导致了运行原生代码时总是异步的这一特征，本文讨论一下RN中异步的一些场景和异步带来的一些问题。</p>',
+        url: '/post',
+      }, {
+        createTime: '2017-09-12 11:33:21',
+        title: 'React Native的异步',
+        html: '<p>RN的JSBridge是一个完成度相当高的东西。它带来了开发速度的巨大提升，相应地也导致了运行原生代码时总是异步的这一特征，本文讨论一下RN中异步的一些场景和异步带来的一些问题。</p>',
+        url: '/post',
+      }],
+    }
+  },
   created() {
     console.log('这是个测试')
   },
