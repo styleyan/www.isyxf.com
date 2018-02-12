90cc279b7fd7d12f7a71cb17e9ce0b1ed1e0ce40
yanxiaofei <forget_love2161@126.com>
2018-02-12 22:48:22 +0800

    fix: 优化项目代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,43 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import routes from './routes'
 
 Vue.use(Router)
 export default new Router({
   mode: 'history',
-  routes,
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: resolve => require(['../views/index/index.vue'], resolve),
+    },
+    {
+      path: '/archive',
+      name: 'archive',
+      component: resolve => require(['../views/archive/index.vue'], resolve),
+    },
+    {
+      path: '/link',
+      name: 'link',
+      component: resolve => require(['../views/link/index.vue'], resolve),
+    },
+    {
+      path: '/article',
+      name: 'article',
+      component: resolve => require(['../views/article/router-view.vue'], resolve),
+      children: [
+        {
+          name: 'article-detail',
+          path: ':articleId',
+          component: resolve => require(['../views/article/detail/index.vue'], resolve),
+        },
+      ],
+    },
+    {
+      path: '/series',
+      name: 'series',
+      component: resolve => require(['../views/series/index.vue'], resolve),
+    },
+  ],
   // 滚动到顶部
   scrollBehavior() {
     return { x: 0, y: 0 }
