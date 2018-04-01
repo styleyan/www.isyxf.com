419a062555a8ae260e6ca20d8fb8a96dc37b81fe
yanxiaofei <forget_love2161@126.com>
2018-04-01 17:02:05 +0800

    fix: 优化eslint检测报错

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'index',
-      component: resolve => require(['../views/index/index.vue'], resolve),
+      component: resolve => require(['../views/index/index-a.vue'], resolve),
     },
     {
       path: '/archive',
